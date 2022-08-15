0d4de312962d3980caa33221fa102a83140c12d6
DeshmukhGanesh <deshmukhganeshb@gmail.com>
2022-08-15 18:50:16 +0530

    emp controller added

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	public void addEmployee() {
+		System.out.println("Employee class added");
+	}
+	
+	
+}
